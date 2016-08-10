40cb755c0d81d3894a639026af05ecd14386d214
Kaitlyn Hova <kaitlynhova@gmail.com>
2016-08-09 22:09:42 -0700

    simple jszip example

---
+++ js/basic.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $( "#download" ).click(function() {
+    var zip = new JSZip();
+    zip.file("Hello.txt", "Hello World\n");
+    zip.generateAsync({type:"blob"})
+    .then(function(content) {
+        // see FileSaver.js
+        saveAs(content, "example.zip");
+    });
+  });
+});
